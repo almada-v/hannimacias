ae79031d6e9b75237efb5ae3bbb3cf4b79907b91
Leonardoo Angel Almada Vidrio <a20200099@utem.edu.mx>
2026-06-19 21:05:57 -0600

    configurar site en astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://www.hmcosmetologa.com', // <--- ESTO ES VITAL
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [sitemap()]
 });
